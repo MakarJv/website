460884800f1bcc251640a98ec7fb18474fd1dbdf
Makar_Jv <makar_igor1997@mail.ru>
2022-10-25 16:36:50 +0300

    add page CardProfile and config theprofile.css and bug fix

--- src/views/TheProfile.vue
+++ src/views/TheProfile.vue
@@ -1,7 +1,28 @@
 <template>
 	<div class="profile">
 		<h2>Profile</h2>
-		<hr>
+		<div class="cardProfile">
+			<ion-icon name="person-sharp"></ion-icon>
+			<ul>
+				<li><a>Name:</a>
+					<br>
+					<span>Igor<ion-icon name="pencil-outline"></ion-icon></span></li>
+				<hr>
+				<li><a>Surname:</a>
+					<br>
+					<span>Makarenko<ion-icon name="pencil-outline"></ion-icon></span></li>
+				<hr>
+				<li><a>Age:</a>
+					<br>
+					<span>25<ion-icon name="pencil-outline"></ion-icon></span></li>
+				<hr>
+				<li><a>About Me:</a>
+					<br>
+					<span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad beatae fuga maiores minus placeat quas! Dolor explicabo illo possimus provident quibusdam reiciendis. Corporis ipsum, nostrum nulla optio quod recusandae voluptas!<ion-icon name="pencil-outline"></ion-icon></span>
+				</li>
+				<hr>
+			</ul>
+		</div>
 	</div>
 </template>
 
